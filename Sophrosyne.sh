4d7daff5abee768119eb710af6f5da19da8fd6b8
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-11-13 21:09:19 +0300

    Update Sophrosyne.sh

--- Sophrosyne.sh
+++ Sophrosyne.sh
@@ -14,35 +14,14 @@ crtsh(){
     TARGET="$1"
 
     if [ -z $1 ]; then
-      echo -e "$OKRED            _         _     $RESET"
-      echo -e "$OKRED   ___ _ __| |_   ___| |__  $RESET"
-      echo -e "$OKRED  / __| '__| __| / __| '_ \ $RESET"
-      echo -e "$OKRED | (__| |  | |_ _\__ \ | | |$RESET"
-      echo -e "$OKRED  \___|_|   \__(_)___/_| |_|$RESET"
-        echo ""
-      echo -e "$OKRED [+] by 1N3@CrowdShield$RESET"
-      echo -e "$OKRED [+] https://crowdshield.com$RESET"
-      echo -e "$OKRED [-] Usage: crt.sh <target>$RESET"
+      echo -e "$OKRED [-] Usage: Sophrosyne.sh <target>$RESET"
       exit
     fi
 
     if [[ $1 = "--help" ]]; then
-      echo -e "$OKRED            _         _     $RESET"
-      echo -e "$OKRED   ___ _ __| |_   ___| |__  $RESET"
-      echo -e "$OKRED  / __| '__| __| / __| '_ \ $RESET"
-      echo -e "$OKRED | (__| |  | |_ _\__ \ | | |$RESET"
-      echo -e "$OKRED  \___|_|   \__(_)___/_| |_|$RESET"
-        echo ""
-      echo -e "$OKRED [-] Usage: crt.sh <target>$RESET"
+      echo -e "$OKRED [-] Usage: Sophrosyne.sh <target>$RESET"
       exit
     fi
-
-    echo -e "$OKRED            _         _     $RESET"
-    echo -e "$OKRED   ___ _ __| |_   ___| |__  $RESET"
-    echo -e "$OKRED  / __| '__| __| / __| '_ \ $RESET"
-    echo -e "$OKRED | (__| |  | |_ _\__ \ | | |$RESET"
-    echo -e "$OKRED  \___|_|   \__(_)___/_| |_|$RESET"
-    echo ""
     echo -e "$OKRED + -- ----------------------------=[Gathering Certificate Subdomains]=-------- -- +$RESET"
     curl -s https://crt.sh/?q=%25.$1 > /tmp/curl.out
     cat /tmp/curl.out | grep $1 | grep TD | sed -e 's/\*\.//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sed -e 's/BR/\n/g' | sort -u > $1-crt.txt
