f91796d5c36e9911dab448d9d27f288cafce69b6
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-11-13 20:33:03 +0300

    Update XSSoter.sh

--- XSSoter.sh
+++ XSSoter.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 COUNTER=0
-for DOMAIN in $(cat rootdomains); do
+for DOMAIN in $(cat $1); do
         python3 /opt/ParamSpider/paramspider.py -d $DOMAIN -o ./$COUNTER-params.txt;
-        let COUNTER=COUNTER+1; 
+        let COUNTER=COUNTER+1;
 done
 
 cat ./*-params.txt > allParams.txt && rm -f ./*-params.txt
 cat allParams.txt | gxss -p talson | dalfox pipe --skip-bav -o ./dalfox-POC.txt
+echo "XSS Soter has finished scanning `basename $PWD` for XSS vulnerabilites..." > report.txt
+echo -n "XSS POCs made: ">> report.txt; cat ./dalfox-POC.txt | wc -l >> report.txt
+notify -data report.txt -bulk && rm report.txt
