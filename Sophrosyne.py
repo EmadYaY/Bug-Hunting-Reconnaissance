7e46ffb445b8e6dc534f1b2aea45299ea9c7631a
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-11-13 20:48:39 +0300

    Update Sophrosyne.py

--- Sophrosyne.py
+++ Sophrosyne.py
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-sophrosyne(){
-    mkdir crt && cd crt
-    bash /opt/crt.sh $1
-    cat * | anew ../rootdomains
-    cd ../
-    rm -rf crt
-    sed -r 's/<//g' rootdomains | grep -v '@' | sort -u > temp; mv temp rootdomains
-    grep -Ev '\.com[a-zA-Z]' rootdomains > temp; mv temp rootdomains
-}
-
 crtsh(){
     OKBLUE='\033[94m'
     OKRED='\033[91m'
@@ -38,8 +28,6 @@ crtsh(){
       echo -e "$OKRED | (__| |  | |_ _\__ \ | | |$RESET"
       echo -e "$OKRED  \___|_|   \__(_)___/_| |_|$RESET"
         echo ""
-      echo -e "$OKRED [+] by 1N3@CrowdShield$RESET"
-      echo -e "$OKRED [+] https://crowdshield.com$RESET"
       echo -e "$OKRED [-] Usage: crt.sh <target>$RESET"
       exit
     fi
@@ -50,12 +38,20 @@ crtsh(){
     echo -e "$OKRED | (__| |  | |_ _\__ \ | | |$RESET"
     echo -e "$OKRED  \___|_|   \__(_)___/_| |_|$RESET"
     echo ""
-    echo -e "$OKRED [+] by 1N3@CrowdShield$RESET"
-    echo -e "$OKRED [+] https://crowdshield.com$RESET"
     echo -e "$OKRED + -- ----------------------------=[Gathering Certificate Subdomains]=-------- -- +$RESET"
     curl -s https://crt.sh/?q=%25.$TARGET > /tmp/curl.out
     cat /tmp/curl.out | grep $TARGET | grep TD | sed -e 's/\*\.//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sed -e 's/BR/\n/g' | sort -u > $TARGET-crt.txt
     cat $TARGET-crt.txt
     echo -e "$OKRED [+] Domains saved to: $TARGET-crt.txt"
     echo -e "$OKRED + -- ----------------------------=[Done!]=----------------------------------- -- +$RESET" 
+}
+
+sophrosyne(){
+    mkdir crt && cd crt
+    bash crtsh $1
+    cat * | anew ../rootdomains
+    cd ../
+    rm -rf crt
+    sed -r 's/<//g' rootdomains | grep -v '@' | sort -u > temp; mv temp rootdomains
+    grep -Ev '\.com[a-zA-Z]' rootdomains > temp; mv temp rootdomains
 }
